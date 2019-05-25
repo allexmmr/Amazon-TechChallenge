95e5e00ce9e963affb1b20e400b5834085145951
allexmmr <allexmmr@gmail.com>
2019-05-25 13:12:27 +1000

    Initial commit

---
+++ Devices/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Devices
+{
+    public class App
+    {
+        public int Id { get; set; }
+        public int MemoryRequired { get; set; }
+        public AppTypeEnum AppType { get; set; }
+    }
+
+    public enum AppTypeEnum
+    {
+        Foreground = 1,
+        Background = 2
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Example 1
+
+            List<List<int>> foregroundAppList1 = new List<List<int>>()
+            {
+                new List<int> { 1, 2 },
+                new List<int> { 2, 4 },
+                new List<int> { 3, 6 }
+            };
+
+            List<List<int>> backgroundAppList1 = new List<List<int>>()
+            {
+                new List<int> { 1, 2 }
+            };
+
+            // Output: [[2,1]]
+            optimalUtilization(
+            7, //deviceCapacity
+            foregroundAppList1,
+            backgroundAppList1);
+
+            #endregion
+
+            #region Example 2
+
+            List<List<int>> foregroundAppList2 = new List<List<int>>()
+            {
+                new List<int> { 1, 3 },
+                new List<int> { 2, 5 },
+                new List<int> { 3, 7 },
+                new List<int> { 4, 10 }
+            };
+
+            List<List<int>> backgroundAppList2 = new List<List<int>>()
+            {
+                new List<int> { 1, 2 },
+                new List<int> { 2, 3 },
+                new List<int> { 3, 4 },
+                new List<int> { 4, 5 }
+            };
+
+            // Output: [[2,4], [3,2]]
+            optimalUtilization(
+            10, //deviceCapacity
+            foregroundAppList2,
+            backgroundAppList2);
+
+            #endregion
+        }
+
+        public static List<List<int>> optimalUtilization(int deviceCapacity,
+                                                List<List<int>> foregroundAppList,
+                                                List<List<int>> backgroundAppList)
+        {
+            // Foreground Apps
+            List<App> foregroundList = new List<App>();
+
+            foregroundAppList.ForEach(app =>
+                foregroundList.Add(new App
+                {
+                    Id = app[0],
+                    MemoryRequired = app[1],
+                    AppType = AppTypeEnum.Foreground
+                }));
+
+            // Background Apps
+            List<App> backgroundList = new List<App>();
+
+            backgroundAppList.ForEach(app =>
+                backgroundList.Add(new App
+                {
+                    Id = app[0],
+                    MemoryRequired = app[1],
+                    AppType = AppTypeEnum.Background
+                }));
+
+            // Order apps by memory required
+            foregroundList = foregroundList.OrderByDescending(q => q.MemoryRequired).ToList();
+            backgroundList = backgroundList.OrderByDescending(q => q.MemoryRequired).ToList();
+
+            List<List<int>> result = new List<List<int>>();
+            int currentCapacity = 0;
+
+            foreach (App foregroundApp in foregroundList)
+            {
+                foreach (App backgroundApp in backgroundList)
+                {
+                    int sum = foregroundApp.MemoryRequired + backgroundApp.MemoryRequired;
+
+                    if (sum <= deviceCapacity)
+                    {
+                        // Add an item to the result list
+                        result.Add(new List<int> { foregroundApp.Id, backgroundApp.Id });
+
+                        if (sum != currentCapacity)
+                        {
+                            // Increase the current capacity
+                            currentCapacity = currentCapacity + sum;
+                        }
+                    }
+
+                    if (currentCapacity > deviceCapacity)
+                    {
+                        // Remove an item if there is not enough capacity
+                        result.RemoveAt(result.Count - 1);
+                        // Decrease the current capacity
+                        currentCapacity = currentCapacity - sum;
+                    }
+                }
+            }
+
+            // Reverse the list
+            result.Reverse();
+            
+            return result;
+        }
+    }
+}
---
+++ Steakhouses/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Steakhouses
+{
+    public class Steakhouse
+    {
+        public string Name { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            nearestXsteakHouses(
+                3,
+                new int[,] { { 1, -3 }, { 1, 2 }, { 3, 4 } },
+                1);
+        }
+
+        public static List<List<int>> nearestXsteakHouses(int totalSteakhouses,
+                                            int[,] allLocations,
+                                               int numSteakhouses)
+        {
+            List<Steakhouse> steakhouseList = new List<Steakhouse>();
+
+            for (int i = 0; i < totalSteakhouses; i++)
+            {
+                steakhouseList.Add(new Steakhouse
+                {
+                    Name = $"Steakhouse #{i + 1}",
+                    Latitude = allLocations[i, 0],
+                    Longitude = allLocations[i, 1]
+                });
+            }
+
+            Steakhouse currentLocation = new Steakhouse
+            {
+                Latitude = 0,
+                Longitude = 0
+            };
+
+            List<Steakhouse> nearByPlaces = steakhouseList
+                .Where(q => q != currentLocation).
+                OrderBy(q => CalculateDistance(currentLocation, q)).
+                Take(numSteakhouses)
+                .ToList();
+
+            if (nearByPlaces == null)
+                return null;
+
+            return nearByPlaces.
+                ConvertAll(q => new List<int> { q.Latitude, q.Longitude });
+        }
+
+        private static double CalculateDistance(Steakhouse currentLocation,
+                                    Steakhouse targetLocation)
+        {
+            return Math.Pow(targetLocation.Latitude - currentLocation.Latitude, 2) + Math.Pow(targetLocation.Longitude - currentLocation.Longitude, 2);
+        }
+    }
+}
